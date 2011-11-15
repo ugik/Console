28e5eba95f4cd909f95538a98023632486ff4f13
GK <georgek@gmail.com>
2011-11-14 21:57:15 -0500

    rspec cleanup

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,11 @@
 Factory.define :admin do |admin|
-  admin.name                  "John Smith"
-  admin.email                   "smith@example.com"
-  admin.password             "foobar"
-  admin.password_confirmation "foobar"
+	admin.name                  "John Smith"
+	admin.email                   "smith@example.com"
+	admin.password             "foobar"
+	admin.password_confirmation "foobar"
 end
 
 Factory.sequence :email do |n|
-  "person-#{n}@example.com"
+	"person-#{n}@example.com"
 end
 
